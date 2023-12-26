b1d4fbafa4e24eb869c02706b8a73197d5ffb888
fangbo13 <104145981+fangbo13@users.noreply.github.com>
2023-12-27 02:38:42 +0800

    add Navbar and it's animation ,change stock code position,wave color and height,add Trinity Logo.Fix bug!

--- InvestiWise/home_react/src/App.js
+++ InvestiWise/home_react/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import HomePage from './components/HomePage/HomePage';  // 确保路径正确
+import Navbar from './components/Navbar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      <Navbar />
     </div>
   );
 }
---
+++ InvestiWise/home_react/src/components/Navbar/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Navbar.css'; // 导入组件的CSS样式
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <>
+        {/* Logo 容器 */}
+        <div className="logo-container">
+          <img src="/img/Logo.png" alt="header-Logo" className="logo" />
+        </div>
+
+        {/* 导航条 */}
+        <nav className="navbar navbar-expand-lg">
+          <div className="collapse navbar-collapse" id="navbarText">
+            {/* 导航链接 */}
+            <ul className="navbar-nav ml-auto line">
+              <li className="nav-item">
+                <a className="nav-link active" href="#home">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#about">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#services">Services</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#tutorials">Tutorials</a>
+              </li> 
+            </ul>
+          </div>  
+        </nav>
+      </>
+    );
+  }
+}
+
+export default Navbar;
